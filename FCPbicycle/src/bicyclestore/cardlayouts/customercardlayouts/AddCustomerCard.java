c3a5a9e5feb4b97abdf206d7a69813b6847fb604
colmfeely <colm.feely@gmail.com>
2015-09-02 01:02:57 +0100

    Update AddCustomerCard.java

--- FCPbicycle/src/bicyclestore/cardlayouts/customercardlayouts/AddCustomerCard.java
+++ FCPbicycle/src/bicyclestore/cardlayouts/customercardlayouts/AddCustomerCard.java
@@ -1,15 +1,13 @@
-package bicyclestore.cardlayouts.customercardlayouts;
+package bicyclestore.cardlayouts.bicyclelayout;
 
-
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
-import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -20,170 +18,170 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 
 import bicyclestore.Database;
-import bicyclestore.customers.Customer;
+import bicyclestore.bikes.BMX;
+import bicyclestore.bikes.Bicycle;
+import bicyclestore.cardlayouts.customercardlayouts.CustomersCardLayout;
 
-public class AddCustomerCard extends JPanel {
-	
-	// auto generated serialVersionUID
-	private static final long serialVersionUID = 3323602430099342409L;
 
+
+public class AddBicycles extends JPanel  {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Database database;
-	private CustomersCardLayout cardLayout;
+	private BicycleTableSorter bicycleTableSorter;
+	private BicycleCardLayout bicycleCardLayout;
+	private JTextField txtType, txtModel, txtColour, txtFrameSize, txtWheelSize,txtFrameComposition,txtCostPrice,txtSalePrice;
+	private JButton addBicycleButton;
+	private ArrayList<Bicycle> newArrayList; 
 	
-	private JTextField txtId, txtName, txtAddress, txtPhone, txtEmail;
-	private JButton btnAddCustomer;
-	
-	private JButton btnEditId;
-	private Box idPane;
-	private int idCounter = 5;
-
-	public AddCustomerCard(Database database, CustomersCardLayout cardLayout) {
+	public AddBicycles(Database database,BicycleCardLayout bicycleCardLayout ) {
+		
 		this.database = database;
-		this.cardLayout = cardLayout;
+		this.bicycleCardLayout = bicycleCardLayout;
+		
 		initComponents();
 		createAddCustomerCard();
+		
+		
 	}
-	
 	private void initComponents() {
-		setUpIdPane();
+	
 		
-		txtName = new JTextField(10);
-		txtAddress = new JTextField(10);
-		txtPhone = new JTextField(10);
-		txtEmail = new JTextField(10);
+		txtType = new JTextField(10);
+		txtModel = new JTextField(10);
+		txtColour = new JTextField(10);
+		txtFrameSize = new JTextField(10);
+		txtWheelSize = new JTextField(10);
+		txtFrameComposition = new JTextField(10);
+		txtCostPrice = new JTextField(10);
+		txtSalePrice = new JTextField(10);
 		
-		btnAddCustomer = new JButton("Add Customer");
-		btnAddCustomer.setAlignmentX(Component.CENTER_ALIGNMENT);
-		btnAddCustomer.addActionListener(new ButtonActionListener());
+		addBicycleButton = new JButton("Add Bicycle");
+		addBicycleButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		addBicycleButton.addActionListener(new ButtonActionListener());
 	}
 	
-	private void setUpIdPane() {
-		idPane = Box.createHorizontalBox();
-		
-		// set ID field to auto-increment value, grey out colour and set editable to false
-		txtId = new JTextField(idCounter+"",10);
-		txtId.setBackground(Color.LIGHT_GRAY);
-		txtId.setEditable(false);
-		
-		btnEditId = new JButton("Edit ID");
-		
-		idPane.add(txtId);
-		idPane.add(btnEditId);
-		
-		btnEditId.addActionListener(new ButtonActionListener());
-	}
-	
-	private void createAddCustomerCard() {
-		TitledBorder addCustomerBorder = BorderFactory.createTitledBorder("Enter Customer Details");
-		addCustomerBorder.setTitleJustification(TitledBorder.CENTER);
-		setBorder(addCustomerBorder);
+	public void createAddCustomerCard(){
+		TitledBorder addBicycleBorder = BorderFactory.createTitledBorder("Enter Bicycle Details");
+		addBicycleBorder.setTitleJustification(TitledBorder.CENTER);
+		setBorder(addBicycleBorder);
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		
-		add(createCustomerDetailsForm());
-		add(btnAddCustomer);
+		add(createBicycleDetailsForm());
+		add(addBicycleButton);
 	}
-	
-	private JPanel createCustomerDetailsForm() {
-		JPanel customerDetailsGrid = new JPanel();
-		GridLayout gridLayout = new GridLayout(5,2);
+	public JPanel createBicycleDetailsForm(){
+		
+		JPanel bicycleDetailsGrid = new JPanel();
+		GridLayout gridLayout = new GridLayout(8,2);
 		gridLayout.setVgap(10);
-		customerDetailsGrid.setBorder(new EmptyBorder(20,20,20,20));
-		customerDetailsGrid.setLayout(gridLayout);
-		customerDetailsGrid.setMaximumSize(new Dimension(400, 400));
-		customerDetailsGrid.add(new JLabel("Customers id"));
-		customerDetailsGrid.add(idPane);
-		customerDetailsGrid.add(new JLabel("Customers name"));
-		customerDetailsGrid.add(txtName);
-		customerDetailsGrid.add(new JLabel("Customers address"));
-		customerDetailsGrid.add(txtAddress);
-		customerDetailsGrid.add(new JLabel("Customers phone number"));
-		customerDetailsGrid.add(txtPhone);
-		customerDetailsGrid.add(new JLabel("Customers email"));
-		customerDetailsGrid.add(txtEmail);
+		bicycleDetailsGrid.setBorder(new EmptyBorder(20,20,20,20));
+		bicycleDetailsGrid.setLayout(gridLayout);
+		bicycleDetailsGrid.setMaximumSize(new Dimension(400, 400));
 		
-		return customerDetailsGrid;
-	}
-	
-	// check if all fields have been filled and attempt to add customer to database
-	private void checkFieldsComplete() {
-		// get customer details from text fields
-		String strId = txtId.getText();
-		String strName = txtName.getText();
-		String strAddress = txtAddress.getText();
-		String strPhone = txtPhone.getText();
-		String strEmail = txtEmail.getText();
+		bicycleDetailsGrid.add(new JLabel("Bicycle Type"));
+		bicycleDetailsGrid.add(txtType);
+		bicycleDetailsGrid.add(new JLabel("Bicycle Model"));
+		bicycleDetailsGrid.add(txtModel);
+		bicycleDetailsGrid.add(new JLabel("Bicycle Colour"));
+		bicycleDetailsGrid.add(txtColour);
+		bicycleDetailsGrid.add(new JLabel("Bicycle Frame Size"));
+		bicycleDetailsGrid.add(txtFrameSize);
+		bicycleDetailsGrid.add(new JLabel("Bicycle Wheel Size"));
+		bicycleDetailsGrid.add(txtWheelSize);
+		bicycleDetailsGrid.add(new JLabel("Bicycle Frame Composition"));
+		bicycleDetailsGrid.add(txtFrameComposition);
+		bicycleDetailsGrid.add(new JLabel("Bicycle Cost Price"));
+		bicycleDetailsGrid.add(txtCostPrice);
+		bicycleDetailsGrid.add(new JLabel("Bicycle Sale Price"));
+		bicycleDetailsGrid.add(txtSalePrice);
 		
-		// check if any text fields have been left blank and output error message if true
-		if(strId.length() == 0 || strName.length() == 0 || strAddress.length() == 0 
-				|| strPhone.length() == 0 || strEmail.length() == 0) {
-			JOptionPane.showMessageDialog(null, "You must enter all customer details before submitting", 
-					"Incomplete content", JOptionPane.ERROR_MESSAGE);
-		}
-		// attempt to add customer if all fields are complete
-		else
-			addCustomer(strId, strName, strAddress, strPhone, strEmail);
-	}
-	
-	private void addCustomer(String strId, String name, String address, String phoneNum, String email) {
-		// attempt to parse customer id integer from string
-		int customerId = 0;
-		boolean gotId = false;
-		try{
-			customerId = Integer.parseInt(strId);
-			gotId = true;
-		}
-		catch(Exception e) {
-			JOptionPane.showMessageDialog(null, "The customer id field must not contain any non numeric characters",
-					"Invalid content", JOptionPane.ERROR_MESSAGE);
-		}
-		if(gotId) {
-			// If the customer was added with auto-increment id, increment id for next customer 
-			if(customerId == idCounter)
-				idCounter++;
-			database.addCustomer(new Customer(customerId, name, address, phoneNum, email));
-			confirmCustomerAdded(strId, name, address, phoneNum, email);
-		}
-	}
-	
-	private void confirmCustomerAdded(String strId, String name, String address, String phone, String email) {
-		// display information message and reset all text fields to blank
-		JOptionPane.showMessageDialog(null, "New customer added,\nID: "+strId+", Name: "+name+", Address: "+address+
-				"\n Phone number: "+phone+", Email: "+email,
-				"Success", JOptionPane.INFORMATION_MESSAGE);
-		txtId.setText(idCounter+"");
-		txtName.setText("");
-		txtAddress.setText("");
-		txtPhone.setText("");
-		txtEmail.setText("");
+		return bicycleDetailsGrid;
 		
-		cardLayout.newCustomerAdded(name);
+		}
+	public void checkFieldsComplete(){
+		
+		// get bicycle details from text fields
+				String type = txtType.getText();
+				String model = txtModel.getText();
+				String colour = txtColour.getText();
+				String frameSize = txtFrameSize.getText();
+				String wheelSize = txtWheelSize.getText();
+				String frameComposition = txtFrameComposition.getText();
+				String costPrice = txtCostPrice.getText();
+				String salePrice = txtSalePrice.getText();
+				
+				int intFrameSize;
+				int intWheelSize;
+				double doubleCostPrice;
+				double doubleSalePrice;
+				intFrameSize = Integer.parseInt(frameSize);
+				intWheelSize = Integer.parseInt(wheelSize);
+				doubleCostPrice = Double.parseDouble(costPrice);
+				doubleSalePrice = Double.parseDouble(salePrice);
+				 
+				
+				// check if any text fields have been left blank and output error message if true
+				if(type.length() == 0 || model.length() == 0 || colour.length() == 0 
+						|| frameSize.length() == 0 || wheelSize.length() == 0 || frameComposition.length() == 0 || costPrice.length() == 0 || salePrice.length() == 0 ) {
+					JOptionPane.showMessageDialog(null, "Please Enter All Details", 
+							"Incomplete content", JOptionPane.ERROR_MESSAGE);
+				}
+				// attempt to add bicycle if all fields are complete
+				else if(type.equals("BMX")){
+				
+					addBicycle (model, colour, intFrameSize, intWheelSize,frameComposition,doubleCostPrice,doubleSalePrice);
+				}
+		}
+	
+	public void addBicycle(String model,String colour,int intFrameSize,int intWheelSize,String frameComposition, double doubleCostPrice,double doubleSalePrice  ){
+		
+			
+		//if(type.equals("BMX")){
+			
+		database.addBicycle(new BMX(model, colour, intFrameSize, intWheelSize,frameComposition, doubleCostPrice, doubleSalePrice));
+		
+		//}
+		txtType.setText("");
+		txtModel.setText("");
+		txtColour.setText("");
+		txtFrameSize.setText("");
+		txtWheelSize.setText("");
+		txtFrameComposition.setText("");
+		txtCostPrice.setText("");
+		txtSalePrice.setText("");
+		
+		bicycleCardLayout.newBicyclerAdded();
+		
+		//bicycleTableSorter.refresh();
+		
+	
+		
+		
+		
 	}
 	
+
 	private class ButtonActionListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent event) {
 			
-			// if the user pressed the add customer button, check if fields are all filled and attempt to add customer
-			if(event.getSource() == btnAddCustomer) {
+			// if the user pressed the add bicycle button, check if fields are all filled and attempt to add customer
+			if(event.getSource() == addBicycleButton) {
+				System.out.println("button pressed");
 				checkFieldsComplete();
+			System.out.println("button pressed");
 			}
-			// if the user pressed the edit id button display dialogue allowing them to edit id field
-			if(event.getSource() == btnEditId) {
-				String custIdStr = JOptionPane.showInputDialog(null, "Enter customer ID",
-						"Edit Customer ID", JOptionPane.INFORMATION_MESSAGE);
-				if(custIdStr != null) {
-					try{
-						txtId.setText(Integer.parseInt(custIdStr) + "");
-					}catch(NumberFormatException e) {
-						JOptionPane.showMessageDialog(null, "Customer ID must contain a number",
-							"Invalid Input", JOptionPane.ERROR_MESSAGE);
-					}
-				}
+			
 				
-			}
+			
+	
 		}
-		
 	}
-
+	
 }
+
+	
